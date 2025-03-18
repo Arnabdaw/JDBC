b6ff2a6a1da8d2e3f9e4c3f4a40e4d6c52373257
Arnabdaw <arnabdaw1@gmail.com>
2025-03-19 01:41:38 +0530

    modified

--- Truck-Management-System/src/main/java/org/example/project1.java
+++ Truck-Management-System/src/main/java/org/example/project1.java
@@ -10,42 +10,28 @@ public class project1
     {
        TruckService truckService=new TruckService();
 
-//     System.out.println("1: To add a new truck detail\n2: To display a specific truck detail\n" +
-//             "3: To update a truck detail\n4: To display all truck details\n5: To delete a truck " +
-//             "detail\n ");
-//     while(true)
-//     {
-//        System.out.println("enter your choice");
-//        Scanner obj=new Scanner(System.in);
-//        int ch=obj.nextInt();
-//        switch(ch)
-//        {
-//           case 1:
 //
-//        }
-//     }
 
 
 
+       Truck t1=new Truck("TATA","2017",1000,"Rajesh");
+       Truck t2=new Truck("VOLVO","2019",3500,"Vola");
+       Truck t3=new Truck("MAHENDRA","2014",2000,"Bubai");
+       Truck t4=new Truck("EICHER","2022",400,"Papon");
+       Truck t5=new Truck("HONDA","2011",1300,"Aniket");
+       Truck t6=new Truck("TESLA","2025",5000,"Elon");
+       Truck t7=new Truck("PORCHE","2021",1900,"Rajjaq");
 
-//       Truck t1=new Truck("TATA","2017",1000,"Rajesh");
-//       Truck t2=new Truck("VOLVO","2019",3500,"Vola");
-//       Truck t3=new Truck("MAHENDRA","2014",2000,"Bubai");
-//       Truck t4=new Truck("EICHER","2022",400,"Papon");
-//       Truck t5=new Truck("HONDA","2011",1300,"Aniket");
-//       Truck t6=new Truck("TESLA","2025",5000,"Elon");
-//       Truck t7=new Truck("PORCHE","2021",1900,"Rajjaq");
 
-
-       //adding data in the database
-//        System.out.println("Adding data:");
-//        truckService.addTruck(t1);
-//        truckService.addTruck(t2);
-//        truckService.addTruck(t3);
-//        truckService.addTruck(t4);
-//        truckService.addTruck(t5);
-//        truckService.addTruck(t6);
-//        truckService.addTruck(t7);
+//       adding data in the database
+        System.out.println("Adding data:");
+        truckService.addTruck(t1);
+        truckService.addTruck(t2);
+        truckService.addTruck(t3);
+        truckService.addTruck(t4);
+        truckService.addTruck(t5);
+        truckService.addTruck(t6);
+        truckService.addTruck(t7);
 
 
         //fetching a specific row
